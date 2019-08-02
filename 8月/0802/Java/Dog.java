8aae78f8309d261d95845b76220a8d1334c46934
Wang Zhaorui <3239938564@qq.com>
2019-08-02 18:49:48 +0800

    add files

---
+++ 8月/0802/Java/Dog.java
@@ -0,0 +1,28 @@
+package Animal;
+
+public class Dog extends Animal implements See {
+
+	@Override
+	public void kanjia() {
+		// TODO Auto-generated method stub
+		System.out.println("ø¥º“");
+	}	
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		System.out.println("≥‘π∑¡∏");
+	}
+	
+	@Override
+	public void rr() {
+		// TODO Auto-generated method stub
+		System.out.println("¬Ù√»");
+	}
+public static void main(String[] args) {
+	Dog dog = new Dog();
+	dog.kanjia();
+	
+}
+
+}
+   
---
+++ 8月/0802/Java/See.java
@@ -0,0 +1,6 @@
+package Animal;
+
+public interface See {
+public void kanjia();
+public void eat();
+}
